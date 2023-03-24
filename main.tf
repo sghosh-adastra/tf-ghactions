95b5d6a461cc0f06457f4d4df525af656afc1690
sghosh-adastra <sourish.ghosh@adastragrp.com>
2023-03-24 16:59:24 +0100

    hh

--- main.tf
+++ main.tf
@@ -1,10 +1,7 @@
 terraform {
   required_version = ">= 0.12.24"
 
-  backend "s3" {
-    bucket = "adastra-tsg-ghactions-tf"
-    region = "eu-central-1"
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
